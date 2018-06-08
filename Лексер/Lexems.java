7245f057325a4e22bc3eb51846f2ba06177a1984
ded1ad <bigvasya97@mail.ru>
2018-06-08 17:37:23 +0300

    Add files via upload

---
+++ Лексер/Lexems.java
@@ -0,0 +1,17 @@
+//Created by Romanov Vasiliy 27.04.18 20:48
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lexems {
+
+    static Map<String, String> lexems = new HashMap<String,String>(); // объявление хеш-таблицы для put и get
+
+    public Lexems(){
+        lexems.put("VAR", "^[a-z|A-Z]+$"); // переменная
+        lexems.put("DIGIT", "^0|[1-9][0-9]*$"); // цифра
+        lexems.put("OPERATOR", "^[-|+|/|*]$"); //оператор
+        lexems.put("ASSIGNMENT OPERATOR", "^=$"); //оператор присваивания
+    }
+
+}
---
+++ Лексер/Token.java
@@ -0,0 +1,14 @@
+//Created by Romanov Vasiliy 27.04.18 20:45
+
+public class Token {
+
+    String type;
+    String token;
+
+    Token(String Type, String Token){ // разбиение на символы
+        type = Type;
+        token = Token;
+    }
+}
+
+
